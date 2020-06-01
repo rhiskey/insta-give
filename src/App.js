db8cd9cf7d08c664ac456557542e5d35aa18b454
Vladimir <35545041+rhiskey@users.noreply.github.com>
2020-06-01 22:38:43 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Post from './components/Post';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  //state ={users: []}
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      value: 'Раздача 1'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Раздача: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  componentDidMount() {
+    //this.getUsers();
+
+    //Subscribtion accs
+    let self = this;
+        fetch('https://infinite-tundra-04834.herokuapp.com/accounts', {
+            method: 'GET'
+        }).then(function(response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        }).then(function(data) {
+            self.setState({users: data});
+        }).catch(err => {
+        console.log('caught it!',err);
+        })
+  }
+
+  //showUsers = user => <div key={user.username}>{user.avatar}</div>
+
+  render() {
+    const { users } = this.state;
+    const selvalue = this.state.value;
+    return <div className="App">
+      <Header />
+      <section className="App-main">
+        <div><h1>Giveaway на сегодня: </h1></div>
+
+      <section className ="App-user">
+       <span><h2>Main ЮЗЕР</h2></span>
+        <img border="0" alt="Main user avatar" src="https://www.kinonews.ru/insimgs/2019/newsimg/newsimg87089.jpg" width="120px" height="120px"></img>
+        <span className ="App-give-text"><button>Раздача конвейера говна подпишись на эти аккаунты </button></span>
+      </section>
+
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Выберите РАЗДАЧУ:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Грейпфрут</option>
+            <option value="lime">Лайм</option>
+            <option value="coconut">Кокос</option>
+            <option value="mango">Манго</option>
+          </select>
+        </label>
+        <input type="submit" value="Выбрать" />
+      </form>
+
+      <div>
+        {this.state.users.map(member =>
+        <center><tr key={member.username}>
+        <Post nickname={member.username} avatar={member.avatar} followlink={member.link} />
+
+        </tr></center>
+        )}
+      </div>
+     
+  
+        {/* more posts */}
+      </section>
+{/* 
+      <section className="Mysql">
+
+      <div className="container"> 
+            <div className="panel panel-default p50 uth-panel">
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Member name</th>
+                            <th>Member avatar</th>
+                            <th>Member Link</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.users.map(member =>
+                        <tr key={member.username}>
+                        <td>{member.username} </td>
+                        <td>{member.link}</td>
+                        <td>{member.avatar}</td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+      </section> */}
+
     </div>
-  );
+
+
+  }
 }
 
+// ReactDOM.render(
+//   <App />,
+//   mountNode
+// );
 export default App;
---
+++ src/components/Post/index.js
@@ -0,0 +1,82 @@
+// src/components/Post/index.js
+import React, { Component } from "react";
+// import { Query } from "react-apollo";
+// import gql from "graphql-tag";
+import "./Post.css";
+
+//  const Post = () => {
+//     return (
+//       <Query
+//         query={gql`
+//           {
+//             post(user_id: "a", post_id: "a") {
+//               image
+//               caption
+//               user {
+//                 nickname
+//                 avatar
+//               }
+//             }
+//           }
+//         `}
+//       >
+//     {({ loading, error, data }) => {
+//           if (loading) return <p>Loading Post...</p>;
+//           if (error) return <p>Error loading Post:(</p>;
+//           let image = data.post.image;
+//           let caption = data.post.caption;
+//           let user = data.post.user;
+//           return (
+//             <article className="Post" ref="Post">
+//               <header>
+//                 <div className="Post-user">
+//                   <div className="Post-user-avatar">
+//                     <img src={user.avatar} alt={user.nickname} />
+//                   </div>
+//                   <div className="Post-user-nickname">
+//                     <span>{user.nickname}</span>
+//                   </div>
+//                 </div>
+//               </header>
+//               <div className="Post-image">
+//                 <div className="Post-image-bg">
+//                   <img alt={caption} src={image} />
+//                 </div>
+//               </div>
+//               <div className="Post-caption">
+//                 <strong>{user.nickname}</strong> {caption}
+//               </div>
+//             </article>
+//           );
+//         }}
+//       </Query>
+//     );
+//   };
+
+//   export default Post;
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    const nickname = this.props.nickname;
+    const avatar = this.props.avatar;
+    //const image = this.props.image;
+    const caption = this.props.caption;
+    const followlink = this.props.followlink;
+
+    return (
+      <article className="Post" ref="Post">
+        <img src={avatar} alt={nickname} className="Post-user-avatar" />
+        <span className="Post-user-nickname">{nickname}</span>
+        <strong>{caption}</strong> 
+        <a href={followlink} className="Post-follow-link" ><img border="0" alt="FollowUser" src="https://i.pinimg.com/originals/a4/a3/1a/a4a31ada1473bb2bf2f05cbb43698dcb.png" width="120px" height="50px"></img></a>
+      </article>
+    );
+    
+  }
+}
+export default Post;
+

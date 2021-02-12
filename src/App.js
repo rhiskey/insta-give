6feff05ee3bdfeb519a3170102b10dad23671411
Vladimir Kiselev <first3395@gmail.com>
2021-02-12 13:17:21 +0300

    modified router

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ class App extends Component {
           <Header />
           <MiniMenu />
 
-          <BrowserRouter>
+          <BrowserRouter basename="/insta-give">
             <div>
 
               <Switch>
